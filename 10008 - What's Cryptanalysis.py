b49ac722be223f26499335882c4a0cc966798882
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2022-01-23 17:57:44 +0800

    Update 10008 - What's Cryptanalysis.py

--- 10008 - What's Cryptanalysis.py
+++ 10008 - What's Cryptanalysis.py
@@ -1,8 +1,4 @@
-import collections
-data=str()
-for t in range(int(input())):
-    t1=input().upper()
-    data+=t1
-didi={t2:data.count(t2) for t2 in data if t2.isalpha()}
-b = sorted(didi.items(), key = lambda d:d[1], reverse = True)
-for f,s in b:print(f"{f} {s}")
+from collections import Counter
+co=Counter()
+for t in range(int(input())):co+=Counter(input().upper())
+for f,s in [(k,a) for k,a in list(co.most_common(len(co))) if str(k).isalpha()]:print(f,s)
