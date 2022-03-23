82f9042a3d0210c28d76ff9c790365b59201a7fd
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2022-03-23 11:20:47 +0800

    Update 10929 - You can say 11.py

--- 10929 - You can say 11.py
+++ 10929 - You can say 11.py
@@ -1,11 +1,4 @@
-'''
-Author: your name
-Date: 2022-03-23 10:06:08
-LastEditTime: 2022-03-23 10:12:42
-LastEditors: Please set LastEditors
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-FilePath: \程式\python\10929 - You can say 11.py
-'''
+
 while True:
     a=int(input())
     if a>0:print(f"{a} is a multiple of 11.") if a%11==0 else print(f"{a} is not a multiple of 11.")
