9230f49922fff3ebecd100180f4ba5d2c02cc1a1
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2022-03-27 22:44:22 +0800

    Update 10252 - Common Permutation.py

--- 10252 - Common Permutation.py
+++ 10252 - Common Permutation.py
@@ -1,7 +1,4 @@
 from collections import Counter
 while True:
-    try:
-        a=Counter(set(input()))
-        b=Counter(set(input()))
-        for t in sorted([f for f,s in list(Counter(a+b).most_common(len(a)+len(b))) if s>=2]):print(t,end='')
+    try:for t in sorted([f for f,s in list(Counter(Counter(set(input()))+Counter(set(input()))).most_common(len(a)+len(b))) if s>=2]):print(t,end='')
     except EOFError:break
