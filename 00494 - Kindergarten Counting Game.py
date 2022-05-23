87887bd4e33f6fa231029e08e94b637a2b41b914
李軒豪 <72302793+hibana2077@users.noreply.github.com>
2022-05-23 23:33:18 +0800

    Update 00494 - Kindergarten Counting Game.py

--- 00494 - Kindergarten Counting Game.py
+++ 00494 - Kindergarten Counting Game.py
@@ -1,2 +1,2 @@
 import sys,itertools
-for line in sys.stdin:print(len(list(k for k,t in itertools.groupby(line,str.isalpha)if k==True)))
+for line in sys.stdin:print(len(list(k for k,_ in itertools.groupby(line,str.isalpha)if k==True)))
